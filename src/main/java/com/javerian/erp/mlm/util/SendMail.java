9faf716a32c1121e3721692f376ef9c4d9526a68
Pawan Mishra <pawmishra@gmail.com>
2018-04-29 18:38:16 +0530

    db backup updated for cappingperlevel

--- src/main/java/com/javerian/erp/mlm/util/SendMail.java
+++ src/main/java/com/javerian/erp/mlm/util/SendMail.java
@@ -20,25 +20,84 @@ public class SendMail {
 	@Autowired
 	private Environment environment;
 
-	public void sendMail(String subject, String textMessage, String toEmailAddress, String fromEmailAddress) {
+	// public void sendMail(String subject, String textMessage, String
+	// toEmailAddress, String fromEmailAddress) {
+	//
+	// final String username = environment.getRequiredProperty("mail.username");
+	// final String password = environment.getRequiredProperty("mail.password");
+	//
+	// // Properties props = new Properties();
+	// // props.put("mail.smtp.auth", "true");
+	// // props.put("mail.smtp.starttls.enable", "true");
+	// // props.put("mail.smtp.host", "smtp.gmail.com");
+	// // props.put("mail.smtp.port", "587");
+	//
+	// Properties properties = new Properties();
+	//
+	// properties.put("mail.smtp.auth",
+	// environment.getRequiredProperty("mail.smtp.auth"));
+	// properties.put("mail.smtp.starttls.enable",
+	// environment.getRequiredProperty("mail.smtp.starttls.enable"));
+	// properties.put("mail.smtp.host",
+	// environment.getRequiredProperty("mail.smtp.host"));
+	// properties.put("mail.smtp.port",
+	// environment.getRequiredProperty("mail.smtp.port"));
+	//
+	// Session session = Session.getInstance(properties, new
+	// javax.mail.Authenticator() {
+	// protected PasswordAuthentication getPasswordAuthentication() {
+	// return new PasswordAuthentication(username, password);
+	// }
+	// });
+	//
+	// try {
+	//
+	// Message message = new MimeMessage(session);
+	// // message.setFrom(new InternetAddress("from-email@gmail.com"));
+	// // message.setRecipients(Message.RecipientType.TO,
+	// // InternetAddress.parse("to-email@gmail.com"));
+	// // message.setSubject("Testing Subject");
+	// // message.setText("Dear Mail Crawler," + "\n\n No spam to my email,
+	// please!");
+	//
+	// message.setFrom(new InternetAddress(fromEmailAddress));
+	// message.setRecipients(Message.RecipientType.TO,
+	// InternetAddress.parse(toEmailAddress));
+	// message.setSubject(subject);
+	// message.setText(textMessage);
+	//
+	// Transport.send(message);
+	//
+	// System.out.println("Done");
+	//
+	// } catch (MessagingException e) {
+	// throw new RuntimeException(e);
+	// }
+	// }
 
+	public void sendMail(String subject, String textMessage, String toEmailAddress, String fromEmailAddress) {
+		// final String username = "yourmail@gmail.com";
+		// final String password = "yourpassword";
+		//
 		final String username = environment.getRequiredProperty("mail.username");
 		final String password = environment.getRequiredProperty("mail.password");
 
-		// Properties props = new Properties();
-		// props.put("mail.smtp.auth", "true");
-		// props.put("mail.smtp.starttls.enable", "true");
-		// props.put("mail.smtp.host", "smtp.gmail.com");
-		// props.put("mail.smtp.port", "587");
-
 		Properties properties = new Properties();
+		// props.put("mail.smtp.host", "smtp.gmail.com");
+		// props.put("mail.smtp.socketFactory.port", "465");
+		// props.put("mail.smtp.socketFactory.class",
+		// "javax.net.ssl.SSLSocketFactory");
+		// props.put("mail.smtp.auth", "true");
+		// props.put("mail.smtp.port", "465");
 
 		properties.put("mail.smtp.auth", environment.getRequiredProperty("mail.smtp.auth"));
-		properties.put("mail.smtp.starttls.enable", environment.getRequiredProperty("mail.smtp.starttls.enable"));
+		properties.put("mail.smtp.socketFactory.port", environment.getRequiredProperty("mail.smtp.socketFactory.port"));
+		properties.put("mail.smtp.socketFactory.class",
+				environment.getRequiredProperty("mail.smtp.socketFactory.class"));
 		properties.put("mail.smtp.host", environment.getRequiredProperty("mail.smtp.host"));
 		properties.put("mail.smtp.port", environment.getRequiredProperty("mail.smtp.port"));
 
-		Session session = Session.getInstance(properties, new javax.mail.Authenticator() {
+		Session session = Session.getDefaultInstance(properties, new javax.mail.Authenticator() {
 			protected PasswordAuthentication getPasswordAuthentication() {
 				return new PasswordAuthentication(username, password);
 			}
@@ -47,12 +106,6 @@ public class SendMail {
 		try {
 
 			Message message = new MimeMessage(session);
-			// message.setFrom(new InternetAddress("from-email@gmail.com"));
-			// message.setRecipients(Message.RecipientType.TO,
-			// InternetAddress.parse("to-email@gmail.com"));
-			// message.setSubject("Testing Subject");
-			// message.setText("Dear Mail Crawler," + "\n\n No spam to my email, please!");
-
 			message.setFrom(new InternetAddress(fromEmailAddress));
 			message.setRecipients(Message.RecipientType.TO, InternetAddress.parse(toEmailAddress));
 			message.setSubject(subject);
@@ -60,7 +113,7 @@ public class SendMail {
 
 			Transport.send(message);
 
-			System.out.println("Done");
+			System.out.println("Mail sent succesfully!");
 
 		} catch (MessagingException e) {
 			throw new RuntimeException(e);
